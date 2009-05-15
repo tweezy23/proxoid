2ff575b4405f3767d22915c069fdaa9955c54eff
mike.baroukh <mike.baroukh@16054664-3da9-11de-b48d-dbaac48c8fd3>
2009-05-15 20:22:49 +0000

    Passage du proxy en service

--- proxoid/src/com/proxoid/Proxoid.java
+++ proxoid/src/com/proxoid/Proxoid.java
@@ -1,95 +1,62 @@
 package com.proxoid;
 
 import android.app.AlertDialog;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.mba.proxylight.ProxyLight;
-import com.mba.proxylight.Request;
-import com.mba.proxylight.RequestFilter;
-
-public class Proxoid extends PreferenceActivity implements OnSharedPreferenceChangeListener {
+public class Proxoid extends PreferenceActivity implements OnSharedPreferenceChangeListener, ServiceConnection {
 
 	private static final String TAG = "proxoid";
 	
-	private static final String KEY_PREFS		= "proxoidv1";
+	protected static final String KEY_PREFS		= "proxoidv6";
 	
-	private static final String KEY_ONOFF		= "onoff";
-	private static final String KEY_PORT		= "port";
-	private static final String KEY_USERAGENT	= "useragent";
-	private static final String KEY_PROXY		= "proxy";
+	protected static final String KEY_ONOFF		= "onoff";
+	protected static final String KEY_PORT		= "port";
+	protected static final String KEY_USERAGENT	= "useragent";
 	
-	private static final String USERAGENT_ASIS		= "asis";
-	private static final String USERAGENT_REPLACE	= "replace";
-	private static final String USERAGENT_REMOVE	= "remove";
-
-	private static final String PROXY_NONE			= "none";
-	private static final String PROXY_APN			= "apn";
-
-	private String useragent = null;
-	private String remoteProxy = null;
+	protected static final String USERAGENT_ASIS	= "asis";
+	protected static final String USERAGENT_REPLACE	= "replace";
+	protected static final String USERAGENT_REMOVE	= "remove";
 	
-	private ProxyLight proxy = null;
+	protected static final String COMMAND_STOP = "stop";
+	protected static final String COMMAND_START = "start";
+	protected static final String COMMAND_UPDATE = "update";
 	
-	private class UserAgentRequestFilter implements RequestFilter {
-		@Override
-		public boolean filter(Request request) {
-			if (USERAGENT_REPLACE.equals(useragent)) {
-				request.getHeaders().put("User-Agent", "unknown");
-			} else
-			if (USERAGENT_REMOVE.equals(useragent)) {
-				request.getHeaders().remove("User-Agent");
-			}
-			
-			return false;
-		}
-	};
+	private IProxoidControl proxoidControl = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		SharedPreferences sp = getSharedPreferences();
-
-		proxy = new ProxyLight();
-		proxy.getRequestFilters().add(new UserAgentRequestFilter());
-		proxy.setPort(8080);
-
-		// Les valeurs par defaut
-		Editor e = sp.edit();
-		
-		// On ne demarre jamais tout seul ...
-		e.putBoolean(KEY_ONOFF, false);
-		if (sp.getString(KEY_PORT, null)==null) {
-			e.putString(KEY_PORT, "8080");
-		}
-		if (sp.getString(KEY_USERAGENT, null)==null) {
-			e.putString(KEY_USERAGENT, USERAGENT_REPLACE);
-		}
-//		if (sp.getString(KEY_PROXY, null)==null) {
-//			e.putString(KEY_PROXY, PROXY_APN);
-//		}
-		
-		e.commit();
-		
-		sp.registerOnSharedPreferenceChangeListener(this);
+			
+		getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
 		addPreferencesFromResource(R.xml.prefs);
 		
-
-		
 		// Initialiser d'apres les prefs actuelle
-		onSharedPreferenceChanged(sp, KEY_PORT);
-		onSharedPreferenceChanged(sp, KEY_USERAGENT);
-//		onSharedPreferenceChanged(sp, KEY_PROXY);
+		updateLibelle(KEY_PORT);
+		updateLibelle(KEY_USERAGENT);
+		
+		Intent svc = new Intent(this, ProxoidService.class);
+		bindService(svc, this, Context.BIND_AUTO_CREATE);
 	}
+	
 	
 	private SharedPreferences getSharedPreferences() {
 		return super.getSharedPreferences(KEY_PREFS, MODE_PRIVATE);
@@ -99,68 +66,67 @@ public class Proxoid extends PreferenceActivity implements OnSharedPreferenceCha
 		return getSharedPreferences();
 	}
 
+	private boolean recurse = false;
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences pref, String key) {
-		boolean restart = false;
-		
-		if (KEY_ONOFF.equals(key)) {
-			if (pref.getBoolean(key, false)) {
-				try {
-					proxy.start();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			} else {
-				proxy.stop();
-			}
-		} else 
-		if (KEY_PORT.equals(key)) {
-			proxy.setPort(Integer.parseInt(pref.getString(KEY_PORT, "8080")));
-			restart = true;
-			getPreferenceScreen().findPreference(KEY_PORT).setSummary("Listening port. Current value : "+proxy.getPort());
-		} else 
-		if (KEY_USERAGENT.equals(key)) {
-			useragent=pref.getString(KEY_USERAGENT, USERAGENT_ASIS);
-			getPreferenceScreen().findPreference(KEY_USERAGENT).setSummary("User-Agent filtering. Current value : "+
-					(USERAGENT_ASIS.equals(useragent)?"No filtering":
-					USERAGENT_REMOVE.equals(useragent)?"Remove":"Replace with dummy")
-			);
-		} /* else
-		if (KEY_PROXY.equals(key)) {
-			remoteProxy = pref.getString(KEY_PROXY, PROXY_APN);
-			if (PROXY_NONE.equals(remoteProxy)) {
-				proxy.setRemoteProxy(null, -1);
-			} else {
-				Cursor mCursor = getContentResolver().query(Uri.parse("content://telephony/carriers"), new String[] {"proxy", "port"}, "current=1", null, null);
-				if (mCursor!=null) {
-					try {
-				        if (mCursor.moveToFirst()) {
-				            String vproxy = mCursor.getString(0);
-				            if (vproxy!=null) {
-				            	proxy.setRemoteProxy(vproxy, Integer.parseInt(mCursor.getString(1)));
-				            }
-				        }
-					} finally {
-						mCursor.close();
-					}
-				}
-			}
-			restart = true;
-			getPreferenceScreen().findPreference(KEY_PROXY).setSummary("Use a remote proxy. Current value : "+
-					(PROXY_NONE.equals(remoteProxy)?"None":
-						("Use APN ("+(proxy.getRemoteProxyHost()==null?"none":(proxy.getRemoteProxyHost()+":"+proxy.getRemoteProxyPort()))+")")
-					)
-			);			
-		} */
-		
-		if (restart && proxy.isRunning()) {
+
+		if (!recurse) {
+			Log.d(TAG, "NOTIFICATION ?");
+			
+			// Notifier
+			boolean notifyOk = false;
 			try {
-				proxy.stop();
-				proxy.start();
+				notifyOk = proxoidControl==null || proxoidControl.update();
 			} catch (Exception e) {
 				Log.e(TAG, "", e);
 			}
+			if (!notifyOk) {
+				if (KEY_ONOFF.equals(key) && pref.getBoolean(KEY_ONOFF, false)) {
+					try {
+						recurse = true;
+						SharedPreferences sp = getSharedPreferences();
+						Editor e = sp.edit();
+						e.putBoolean(KEY_ONOFF, false);
+						e.commit();
+						Toast.makeText(this, "Error. Proxoid stopped.", Toast.LENGTH_SHORT).show();
+					} finally {
+						recurse=false;
+					}
+				}
+			}
 		}
+		
+		// Mettre a jour le libelle
+		updateLibelle(key);
+	}
+	
+	private void updateLibelle(String key) {
+		System.err.println("update pour "+key);
+		if (KEY_ONOFF.equals(key)) {
+			return;
+		}
+		String summary = null;
+		String value = getSharedPreferences().getString(key, null);
+		if (KEY_PORT.equals(key)) {
+			summary = "Listening port. Current value : "+(value==null?"8080":value);
+		} else
+		if (KEY_USERAGENT.equals(key)) {
+			summary = "User-Agent filtering. Current value : "+
+			(	USERAGENT_ASIS.equals(value)?"No filtering":
+				USERAGENT_REMOVE.equals(value)?"Remove":
+				"Replace with dummy"
+			);
+		}
+		if (summary!=null) {
+			PreferenceScreen ps = getPreferenceScreen();
+			if (ps!=null) {
+				Preference p = ps.findPreference(key);
+				if (p!=null) {
+					p.setSummary(summary);
+				} 
+			}
+		}
+		
 	}
 	
 	@Override
@@ -183,8 +149,45 @@ public class Proxoid extends PreferenceActivity implements OnSharedPreferenceCha
 				}
 			});
 			d.show();			
-			
 		}
 		return super.onMenuItemSelected(featureId, item);
 	}
+
+
+	@Override
+	public void onServiceConnected(ComponentName cn, IBinder binder) {
+		proxoidControl=(IProxoidControl)binder;
+		if (proxoidControl!=null) {
+			try {
+				proxoidControl.update();
+			} catch (RemoteException e) {
+				Log.e(TAG, "", e);
+			}
+		}
+	}
+
+
+	@Override
+	public void onServiceDisconnected(ComponentName cn) {
+		proxoidControl=null;
+	}
+	
+	@Override
+	protected void onDestroy() {
+		unbindService(this);
+		super.onDestroy();
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode==4 && proxoidControl!=null && getSharedPreferences().getBoolean(KEY_ONOFF, false)) {
+			// On ne quitte pas ...
+			Intent i = new Intent(Intent.ACTION_MAIN);
+			i.addCategory(Intent.CATEGORY_HOME);
+			startActivity(i);
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+	
 }
