67624b302f9cf1d2a23f9d629fa48ba7bba2930d
mike.baroukh <mike.baroukh@16054664-3da9-11de-b48d-dbaac48c8fd3>
2009-05-21 08:33:54 +0000

    1.0.5

--- proxylight/src/com/mba/proxylight/RequestFilter.java
+++
@@ -1,9 +0,0 @@
-package com.mba.proxylight;
-
-public interface RequestFilter {
-
-	/**
-	 * Retourne true si le proxy ne doit meme pas repondre ...
-	 */
-	public boolean filter(Request request);
-}
